2d0aa23a31a886cfddc23e595f984e203fbfdccc
jaredmartin5 <jaredm776@cox.net>
2020-12-19 21:55:34 -0600

    hardcoded location

--- Screens/Home.js
+++ Screens/Home.js
@@ -1,16 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import  Header  from '../Components/Header';
 
 export default function HomeScreen() {
+
+  const [info, setInfo] = useState({
+    name: "loading!!",
+    temp: "loading!!"
+  })
+
+  useEffect(() =>{
+    getWeather()
+  },[])
+  
+  const getWeather = () =>{
+    fetch("https://api.openweathermap.org/data/2.5/weather?q=Denham+Springs&APPID=f65ff475cfd5d4715620b0b4731575ee&units=imperial")
+    .then(data=>data.json())
+    .then(results=>{
+      console.log(results)
+      setInfo({
+        name:results.name,
+        temp:results.main.temp
+      })
+    })
+  }
+
+
+
+
   return (
-    <View>
+    <View style={styles.container}>
       <Header />
       <StatusBar style="auto" />
-      <View style={styles.container}>
-      <Text> Enter your city name:</Text>
-      </View>
+      <Text>{info.name}</Text>
+      <Text>{info.temp}°</Text>
     </View>
 
     
@@ -25,4 +49,5 @@ const styles = StyleSheet.create({
   
   },
 });
+
 
