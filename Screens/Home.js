82cd6aeb8bc0b2c3f58eccdb31b83db82fc589a8
jaredmartin5 <jaredm776@cox.net>
2021-01-04 20:44:36 -0600

    gets your location

--- Screens/Home.js
+++ Screens/Home.js
@@ -1,9 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import CurrentPosition from '../Components/Geolocation';
 import  Header  from '../Components/Header';
 import customData from '../keys.json';
+import * as Location from 'expo-location';
+
 
 export default function HomeScreen() {
 
@@ -16,18 +17,21 @@ export default function HomeScreen() {
 
   useEffect(() =>{
     getWeather()
-  },[])
-  
+    
+  },[])  
   const getWeather = () =>{
-    fetch("https://api.openweathermap.org/data/2.5/weather?q=Denham+Springs&APPID=" + customData.OpenWeatherMap + "&units=imperial")
-    .then(data=>data.json())
-    .then(results=>{
-      console.log(results)
-      setInfo({
-        name:results.name,
-        temp:results.main.temp,
-        humidity:results.main.humidity,
-        weather:results.weather[0].main
+    var currentLocation = Location.getCurrentPositionAsync({});
+    currentLocation.then( position => {
+      fetch("https://api.openweathermap.org/data/2.5/weather?lat=" + position.coords.latitude + "&lon=" + position.coords.longitude + "&appid=" + customData.OpenWeatherMap + "&units=imperial")
+      .then(data=>data.json())
+      .then(results=>{
+        console.log(results)
+        setInfo({
+          name:results.name,
+          temp:results.main.temp,
+          humidity:results.main.humidity,
+          weather:results.weather[0].main
+        })
       })
     })
   }
@@ -38,7 +42,7 @@ export default function HomeScreen() {
   return (
     <View style={styles.container}>
       <Header /> 
-      <CurrentPosition /> 
+      {/* <Text><CurrentPosition /></Text> */}
       <StatusBar style="auto" />
       <Text>{info.name} {"\n"}Temperature: {info.temp}° {"\n"}Humidity: {info.humidity}% {"\n"}Weather: {info.weather} </Text>
     </View>
